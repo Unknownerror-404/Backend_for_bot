9199d16e3923942f1848ccc21df4408c57963dab
Unknownerror-404 <aniketkuyate610@gmail.com>
2025-11-01 17:39:28 +0530

    Modify app.run to use environment variable for port
    Updated app.py to include environment variable for port and adjusted app.run parameters.

--- app.py
+++ app.py
@@ -270,7 +270,9 @@ def logout():
     return render_template('index.html', message="Successfully logged out!")
  
 if __name__ == "__main__":
-    app.run(debug=True, ssl_context='adhoc')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
