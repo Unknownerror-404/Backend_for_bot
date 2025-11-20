ed48ee1ea4d9066f84522c4dc2b113f13ab2debf
Unknownerror-404 <aniketkuyate610@gmail.com>
2025-11-20 21:09:32 +0530

    adding information
    Add patient information insertion route and logic

--- app.py
+++ app.py
@@ -253,7 +253,17 @@ def logout():
     session.clear()
     return render_template("index.html", message="Logged out!")
 
-
+@app.route("/info_insert", methods=["post", "get"])
+def info_insert():
+    if request.method == "POST":
+        patient_name = request.form.get("patient_name")
+        patient_id = request.form.get("patient_id")
+        diesease = request.form.get("diesease")
+        disease_info = request.form.get('disease_info')
+        cursor.execute(
+            "INSERT INTO Patient_Info (Patient_Name, Patient_ID, Disease, Disease_Info) VALUES (%s, %s, %s, %s)",
+            ()
+        )
 # -----------------------------------------
 # RUN
 # -----------------------------------------
@@ -261,6 +271,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     print(f"🚀 Flask running on port {port}")
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
